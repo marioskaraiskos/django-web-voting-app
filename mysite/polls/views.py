2202cbd5f966cedf8b1a05bf3bf77c2687f7f10e
marioskaraiskos <marioskaraiskosbusiness@gmail.com>
2024-07-10 23:31:06 +0300

    creating some tests i dont really know what they do

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import get_object_or_404, render
-from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 from .models import Choice, Question
@@ -14,13 +14,23 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -29,5 +39,4 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    # same as above, no changes needed.
-    ...
+    return HttpResponse("You're voting on question %s." % question_id)

548d56491225fa9d70f57fce5f63971a69849923
marioskaraiskos <marioskaraiskosbusiness@gmail.com>
2025-08-14 00:26:40 +0300

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*!cvc+fy&d-13#g3!2-l8fge!%#%6ndsq-4#fk=jn=gtwld349'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mk2002.pythonanywhere.com']
 
 
 # Application definition
